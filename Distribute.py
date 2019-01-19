23922d4380cbec70cd42ad7e16d3b0d5169a36e4
clayton Dembski <cdembski1@gmail.com>
2019-01-19 00:48:47 -0500

    Now returns a distribted set of points corrisponding to a swarm through the given set of goal locations

--- Distribute.py
+++ Distribute.py
@@ -7,26 +7,32 @@ def distributeNodes(NodeList, NumOfSwarms):
         #may be expensive
         NodeLenList.append(math.sqrt((NodeList[NodeIndex][0] - NodeList[NodeIndex+1][0])**2 +
                           (NodeList[NodeIndex][1] - NodeList[NodeIndex+1][1])**2))
-    swarmPlacement = sum(NodeLenList)/NumOfSwarms
+    swarmPlacement = sum(NodeLenList)/(NumOfSwarms-1)
     swarmGoals = []
     for swarmElNum in range(NumOfSwarms):
-        swarmGoals.append(setSwarms(swarmPlacement, swarmElNum+1,NodeLenList, NodeList))
+        swarmGoals.append(setSwarms(swarmPlacement, swarmElNum,NodeLenList, NodeList))
+
+    return swarmGoals
 
 
 def setSwarms(f, x, NodeLenList, NodeList):
     fx = f*x
     index = 0
-    while(fx > NodeLenList[index]):
-        fx -= NodeLenList(index)
+    while(not(math.isclose(fx,NodeLenList[index])) and fx > NodeLenList[index]):
+        fx -= NodeLenList[index]
         index+=1
-    if (fx == NodeLenList[index]):
+    if (math.isclose(fx,NodeLenList[index])):
         return NodeList[index+1]
     else:
-        theta = math.acos((NodeList[index][0]-NodeList[index+1][0])/NodeLenList[index])
-        return [fx*cos(theta), fx*sin(theta)]
+        theta = math.acos((NodeList[index+1][0]-NodeList[index][0])/NodeLenList[index])
+        thetaDeg = theta*180/math.pi
+        return [round((NodeList[index][0] +fx*math.cos(theta)),5), round(NodeList[index][1] + fx*math.sin(theta),5)]
 
 
 def sortNodeList(NodeList):
     NodeList.sort(key=itemgetter(0))
     return NodeList
 
+# if __name__ == "__main__":
+#     NL = [[0,0],[1,1],[2,2],[3,3]]
+#     print(distributeNodes(NL,5))
